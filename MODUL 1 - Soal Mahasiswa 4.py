a0651b59a006a6337d945b12791282fe7d1fd933
L200170187 <47962434+L200170187@users.noreply.github.com>
2019-05-13 21:59:37 +0700

    MODUL-7

--- MODUL 1 - Soal Mahasiswa 4.py
+++
@@ -1,7 +0,0 @@
-def rerata (b):
-    a=len (b)
-    d=0
-    for x in range (a):
-        d+=b[x]
-        c=d/a
-    print (c)
---
+++ MODUL 7 - Soal Mahasiswa 3.py
@@ -0,0 +1,8 @@
+import re
+
+wiki = open("Indonesia.txt", "r")
+teks = wiki.read()
+wiki.close()
+
+print(re.findall(r'di \w+', teks.lower()))
+
